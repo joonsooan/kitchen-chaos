202066b6e7640335603fe1ba0416fd2c6cb39745
joonsooan <joonsooan@gmail.com>
2026-07-03 20:51:57 +0900

    Created player prefab & Ingredient, Recipe Sriptable Objects

---
+++ Assets/Scripts/Data/Ingredient.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient", menuName = "KitchenChaos/Ingredient")]
+public class Ingredient : ScriptableObject
+{
+    public Sprite ingredientIcon;
+    public string ingredientName;
+}
---
+++ Assets/Scripts/Data/IngredientEntry.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public struct IngredientEntry
+{
+    public Ingredient ingredientType;
+    public int amount;
+}
