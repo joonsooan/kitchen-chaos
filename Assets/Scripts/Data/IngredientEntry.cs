8ad90926cbcde3777fb7fdf9930b72aa8950fd37
joonsooan <joonsooan@gmail.com>
2026-07-03 23:29:49 +0900

    Created temporary recipe & ingredient data

--- Assets/Scripts/Data/IngredientEntry.cs
+++ Assets/Scripts/Data/IngredientEntry.cs
@@ -4,5 +4,6 @@ using System;
 public struct IngredientEntry
 {
     public Ingredient ingredientType;
+    // TODO: 조리 방법 관련 필드 추가
     public int amount;
 }
--- Assets/Scripts/Data/RecipeData.cs
+++ Assets/Scripts/Data/RecipeData.cs
@@ -6,5 +6,6 @@ public class RecipeData : ScriptableObject
     public Sprite recipeIcon;
     public string recipeName;
     public IngredientEntry[] ingredients;
-    // TODO: 조리 방법 관련 필드 추가
+    public int moneyReward;
+    public int scoreReward;
 }
