1eaeb679fc22af7328378931ec89c5cc97cdcbe2
joonsooan <joonsooan@gmail.com>
2026-07-04 22:46:49 +0900

    Created new event for disaster UI

--- Assets/Scripts/Disaster/DisasterEvent.cs
+++ Assets/Scripts/Disaster/DisasterEvent.cs
@@ -1,6 +1,24 @@
+using System;
 using UnityEngine;
 
 public abstract class DisasterEvent : MonoBehaviour
 {
-    public abstract void Trigger();
+    [SerializeField] private string disasterName;
+    [SerializeField, TextArea] private string disasterDescription;
+
+    public static event Action<DisasterEvent> OnAnyDisasterTriggered;
+
+    public string DisasterName => disasterName;
+    public string DisasterDescription => disasterDescription;
+    public virtual float Duration => 0f;
+
+    public bool Trigger()
+    {
+        if (!TryTrigger()) return false;
+
+        OnAnyDisasterTriggered?.Invoke(this);
+        return true;
+    }
+
+    protected abstract bool TryTrigger();
 }
